f39926c455c339db739b45b87a9625129dfe37aa
ekas-7 <ekaspreetatwal@gmail.com>
2024-10-14 00:19:31 +0530

    started frontend

---
+++ frontend/src/pages/Signin.tsx
@@ -0,0 +1,7 @@
+const Signin = ()=>{
+    return (
+        <h1>Hello from signin</h1>
+    );
+}
+
+export default Signin;
---
+++ frontend/src/pages/Signup.tsx
@@ -0,0 +1,7 @@
+const Signup = ()=>{
+    return (
+        <h1>Hello from signup</h1>
+    );
+}
+
+export default Signup;
